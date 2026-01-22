d0569f8172ce51e95e3cedb3bc35e08e82e520fd
KanishkaJayakumar <jaikanishka2004@gmail.com>
2026-01-22 11:21:45 +0530

    RDS DB

--- src/main/java/com/pms/leaderboard/config/EventBuffer.java
+++ src/main/java/com/pms/leaderboard/config/EventBuffer.java
@@ -25,62 +25,40 @@ public class EventBuffer {
     @Autowired
     LeaderboardService leaderboardService;
 
-    // public synchronized void add(MessageDTO dto) {
-    //     buffer.add(dto);
-    //     flush();
-    //     if (buffer.size() >= BUFFER_LIMIT) {
-    //         flush();
-    //     }
-    // }
     public synchronized void addAll(List<MessageDTO> list) {
         buffer.addAll(list);
 
-        log.warn("➕ ADDALL size={} bufferNow={}", list.size(), buffer.size());
-        System.out.println("➕ ADDALL size={} bufferNow={}" + list.size() + "   " + buffer.size());
+        log.warn(" ADDALL size={} bufferNow={}", list.size(), buffer.size());;
         log.info("Buffer size after add: {}", buffer.size());
-        System.out.println("Buffer size after add: {}" + buffer.size());
-        // flush();
 
         if (buffer.size() >= BUFFER_LIMIT) {
-            log.info("  🫙🫙 💯BUFFER FULL ({}). Triggering flush.", buffer.size());
-            System.out.println("BUFFER FULL ({}). Triggering flush." + buffer.size());
+            log.info(" BUFFER FULL ({}). Triggering flush.", buffer.size());
             flush();
         }
     }
 
     @Scheduled(fixedRate = 3000)
     public synchronized void timeFlush() {
-        log.info(" 🧵🧵 Scheduled Flush Trigger at {}", System.currentTimeMillis());
-        System.out.println("Scheduled Flush Trigger at {}" + System.currentTimeMillis());
+        log.info(" Scheduled Flush Trigger at {}", System.currentTimeMillis());
         flush();
     }
 
     private synchronized void flush() {
 
-        log.warn(" 🧵🧵🧵 FLUSH THREAD = {}", Thread.currentThread().getName());
-        System.out.println(" 🧵FLUSH THREAD = {}" + Thread.currentThread().getName());
+        log.warn(" FLUSH THREAD = {}", Thread.currentThread().getName());
 
         if (buffer.isEmpty()) {
             return;
         }
 
         log.info("FLUSHING {} items at {}", buffer.size(), System.currentTimeMillis());
-        System.out.println("Buffer flushed at {}" + System.currentTimeMillis());
 
         List<MessageDTO> batch = new ArrayList<>(buffer);
         buffer.clear();
 
         log.debug("Flushing {} events", batch.size());
-        System.out.println("Flushing {} events" + batch.size());
 
         leaderboardService.processBatch(batch);
 
-        // try {
-        //     wsHandler.broadcast(batch);
-        //     log.info("WS sent at {}", System.currentTimeMillis());
-        //     System.out.println("WS sent at {}" + System.currentTimeMillis());
-        // } catch (Exception e) {
-        //     log.warn("WebSocket broadcast failed", e);
-        // }
     }
 }
